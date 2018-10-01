d296ae8e3972b7fc673e7c38f422b1543af92a6d
eagletommy52 <andrewt@Andrews-MacBook-Pro.local>
2018-10-01 12:18:07 -0400

    fix to game start btn settings lockout

--- memory.js
+++ memory.js
@@ -22,7 +22,8 @@ class MatchingGame {
         this.lastItem;
         this.matches = 0;
         this.attempts = 0;
-        this.numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+        this.numArr = [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
         this.realPicsArr = getWebPics(30);
         }
     setupGame(){
